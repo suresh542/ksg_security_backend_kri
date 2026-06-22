4fce1620f821a978cc3b5a1e93796e666c7d8c00
suresh542 <rsureshrajamanickam@gmail.com>
2026-06-22 12:21:24 +0530

    server changed

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
-import 'dotenv/config'
+  import 'dotenv/config'
 import app from './app.js'
 
 const port = process.env.PORT || 3000
+  console.log('ENV loaded:', {
+    EMAIL_HOST: !!process.env.EMAIL_HOST,
+    EMAIL_USER: !!process.env.EMAIL_USER,
+    EMAIL_PASS: !!process.env.EMAIL_PASS,
+    EMAIL_TO: process.env.EMAIL_TO,
+  })
 
-console.log('ENV loaded:', {
-  EMAIL_HOST: !!process.env.EMAIL_HOST,
-  EMAIL_USER: !!process.env.EMAIL_USER,
-  EMAIL_PASS: !!process.env.EMAIL_PASS,
-  EMAIL_TO: !!process.env.EMAIL_TO,
-})
-
-app.listen(port, () => {
-  console.log(`Contact server listening on http://localhost:${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Contact server listening on http://localhost:${port}`)
+  })
